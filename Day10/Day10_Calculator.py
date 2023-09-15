c27b85eeadbf525766ebfeab917c3f4ef0981a9e
SeungHan Moon <msh9911@naver.com>
2023-09-16 01:34:32 +0900

    gd

--- Day10/Day10_Calculator.py
+++ Day10/Day10_Calculator.py
@@ -11,19 +11,22 @@ def subtract(x, y):
 def multiply(x, y):
     return x*y
  
-def divide(X, y):
+def divide(x, y):
     return x/y
 
 operators = {
-    "+" : add(),
-    "-" : subtract(),
-    "*" : multiply(),
-    "/" : divide()
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide
 }
 
+#입력 값 생성
 num1 = int(input("What's the first number?: ")) #파라미터 x에 대응
 num2 = int(input("What's the second number?: ")) #파라미터 y에 대응
 
 choose_op = input("choose operation: ")
+calculate = operators[choose_op]
+print(f"{num1} {choose_op} {num2} = {calculate}.")
 
                 
