3fc2893340b61b5de2648c8b7552b4e4f3bbf9f7
SeungHan Moon <msh9911@naver.com>
2023-09-16 20:05:47 +0900

    gd

--- Day10/Day10_Calculator.py
+++ Day10/Day10_Calculator.py
@@ -15,15 +15,23 @@ def divide(X, y):
     return x/y
 
 operators = {
-    "+" : add(),
-    "-" : subtract(),
-    "*" : multiply(),
-    "/" : divide()
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide
 }
 
 num1 = int(input("What's the first number?: ")) #파라미터 x에 대응
 num2 = int(input("What's the second number?: ")) #파라미터 y에 대응
 
 choose_op = input("choose operation: ")
+
+calculate = operators[choose_op] #연산자 딕셔너리 안의 키 값(연산자)를 고른다.
+answer = calculate(num1, num2)
+
+print(f"The {num1} {choose_op} {num2} = {answer}.")
+
+
+
 
                 
