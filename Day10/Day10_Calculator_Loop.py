a307f2efa1ba3fe32017d092920c7b7bc50a4b2c
SeungHan Moon <msh9911@naver.com>
2023-09-16 20:00:21 +0900

    calculator loop

--- Day10/Day10_Calculator_Loop.py
+++ Day10/Day10_Calculator_Loop.py
@@ -28,13 +28,18 @@ num2 = int(input("What's the second number?: ")) #파라미터 y에 대응
 choose_op = input("choose operation: ")
 calculate = operators[choose_op]
 first_answer = calculate(num1, num2)
+answer = first_answer
 print(f"{num1} {choose_op} {num2} = {first_answer}.")
 
-#calculate 값을 받아서 다시 연산하는 코드를 작성하세요.
-choose_op = input("Choose another operation: ")
-num3 = int(input("What's the another number?: "))
-calculate = operators[choose_op]
-second_answer = calculate(calculate(num1, num2), num3) #calculate는 딕셔너리에 저장된 함수를 실행하는 것이기 때문에 함수로 적어도 된다.
-print(f"{first_answer} {choose_op} {num3} = {second_answer}.")
+#No를 입력하기 전까지 계속해서 연산이 될 수 있게 하세요
+flag = True
+while flag:
+    number = int(input("Enter the Another Number.: "))
+    choose_op = input("Choose Another Operation: ")
+    calculate = operators[choose_op]
+    another_answer = calculate(answer, number)
+    print(f"{}")
+
+
 
                 
