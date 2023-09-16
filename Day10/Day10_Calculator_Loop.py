9f396016f275ac3bc9d9cabbb0b081a1856c8efa
SeungHan Moon <msh9911@naver.com>
2023-09-16 20:23:17 +0900

    계산기 분기 추가

--- Day10/Day10_Calculator_Loop.py
+++ Day10/Day10_Calculator_Loop.py
@@ -23,22 +23,23 @@ operators = {
 
 #입력 값 생성
 num1 = int(input("What's the first number?: ")) #파라미터 x에 대응
-num2 = int(input("What's the second number?: ")) #파라미터 y에 대응
-
-choose_op = input("choose operation: ")
-calculate = operators[choose_op]
-first_answer = calculate(num1, num2)
-answer = first_answer
-print(f"{num1} {choose_op} {num2} = {first_answer}.")
-
-#No를 입력하기 전까지 계속해서 연산이 될 수 있게 하세요
 flag = True
+#No를 입력하기 전까지 계속해서 연산이 될 수 있게 하세요
 while flag:
     number = int(input("Enter the Another Number.: "))
     choose_op = input("Choose Another Operation: ")
     calculate = operators[choose_op]
-    another_answer = calculate(answer, number)
-    print(f"{}")
+    answer = calculate(num1, number)
+    print(f"{num1} {choose_op} {number} = {answer}.")
+    #여기서 이제 계속 할지 말지 분기를 나눠야 한다.
+    ask = input("계속 계산하실꺼면 yes, 그만 계산하실꺼면 no를 입력해주세요.: ")
+    if ask == "yes":
+        num1 = answer
+    elif ask == "no":
+        flag = False
+
+print(f"계산이 끝났습니다! 최종 결과는 {answer} 입니다.")
+
 
 
 
