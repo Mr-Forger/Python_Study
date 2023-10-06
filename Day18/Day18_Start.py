f0464914cf5e89dd597b86edc65d229755dd8113
SeungHan Moon <msh9911@naver.com>
2023-10-06 22:18:22 +0900

    ~

--- Day18/Day18_Start.py
+++ Day18/Day18_Start.py
@@ -1,14 +1,33 @@
 import turtle
+import random
 
 t = turtle.Turtle()
+turtle.colormode(255)
+
+# #랜덤으로 뻗게하기
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+
+# directions = [0, 90, 180, 270]
+# t.pensize(15)
+t.speed("fastest")
+# for _ in range(200):
+#     t.color(random_color())
+#     t.forward(30)
+#     t.setheading(random.choice(directions))
+
+# 작은 원과 큰 원
+n = 5
+for i in range(int(360/n)):
+    t.color(random_color())
+    t.circle(100)
+    t.setheading(t.heading() + n)
+
+
 screen = turtle.Screen()
-t.shape("turtle")
-t.color("red")
-
-#정사각형 만들기
-for i in range(4):
-    t.forward(100)
-    t.left(90)
-
-
 screen.exitonclick()
