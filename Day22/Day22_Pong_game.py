5dd5f10e87f73147033fd7ebac3a45da1005ada8
SeungHan Moon <msh9911@naver.com>
2023-11-01 15:01:20 +0900

    Day22

--- Day22/Day22_Pong_game.py
+++ Day22/Day22_Pong_game.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from Day22.paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,8 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
+scoreboard.scoreboard_update()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -21,13 +24,25 @@ screen.onkey(l_paddle.go_down, "s")
 
 flag = True
 while flag:
-    time.sleep(0.1)
+    time.sleep(ball.ball_speed)
     screen.update()
     ball.move()
 
-    #벽에 부딫혔을때
+    # 벽에 부딫혔을때 튕겨지게 하는 조건
     if ball.ycor() > 280 or ball.ycor() < -280:
-        #튕겨져야댐
-        ball.bounce()
+        ball.y_bounce()
+
+    # 패들에 부딫혔을때 마찬가지로 튕겨지게 하는 조건
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.x_bounce()
+
+    # 패들을 놓쳤을때 초기화 시키는 조건
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_scoring()
+
+    if ball.xcor() < -380:
+        ball.reset_position()  # 해당 함수에서 포지션 x_bounce 메소드만 호출하는데 왜 반대방향으로 갈까 -> x *= -1을 통해 양수 음수를 계속 바꾸기 때문
+        scoreboard.r_scoring()
 
 screen.exitonclick()
--- Day22/ball.py
+++ Day22/ball.py
@@ -8,11 +8,21 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 10
         self.y_move = 10
+        self.ball_speed = 0.1
 
     def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    def bounce(self):
+    def y_bounce(self):
         self.y_move *= -1
+
+    def x_bounce(self):
+        self.x_move *= -1
+        self.ball_speed *= 0.8
+
+    def reset_position(self):
+        self.goto(0, 0) #최초 위치 좌표
+        self.ball_speed = 0.1
+        self.x_bounce()
