a249930acd6e848e0f39e79ea4c325a7f8422630
SeungHan Moon <msh9911@naver.com>
2023-11-08 18:18:06 +0900

    Day25 다람쥐 데이터 분석

--- Day25/Day25_다람쥐 데이터 분석/squirrel_data_analyze.py
+++ Day25/Day25_다람쥐 데이터 분석/squirrel_data_analyze.py
@@ -1,3 +1,20 @@
 import pandas
 
 data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels = len(data[data["Primary Fur Color"] == "Black"])
+print(grey_squirrels)
+print(red_squirrels)
+print(black_squirrels)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels, red_squirrels, black_squirrels]
+}
+
+print(data_dict)
+
+dict_dataframe = pandas.DataFrame(data_dict)
+print(dict_dataframe)
+dict_dataframe.to_csv("squirrel_count.csv")
