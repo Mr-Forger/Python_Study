034474ae43bc81bcd271d658e0b48428a91e8fe1
SeungHan Moon <msh9911@naver.com>
2023-11-22 21:44:34 +0900

    Day26

---
+++ Day26/Day26_Convert_CtoF.py
@@ -0,0 +1,10 @@
+weather_c = eval(input())
+# 🚨 Don't change code above 👆
+
+
+# Write your code 👇 below:
+weather_f = {day: (temp * 9/5) + 32 for (day, temp) in weather_c.items()}
+print(weather_c.items())
+
+
+print(weather_f)
