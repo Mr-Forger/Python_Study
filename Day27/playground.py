c3847e3f6d46ecb91e5eff02b15f2a5a0730fc62
SeungHan Moon <msh9911@naver.com>
2023-11-23 13:16:39 +0900

    Day27

--- Day27/playground.py
+++ Day27/playground.py
@@ -11,12 +11,32 @@ def add(*args):
 print(add(3, 5, 6, 2, 1, 7, 4, 3))
 
 
-def calculate(**kwargs):  # 가변 키워드 인수
+def calculate(n, **kwargs):  # 가변 키워드 인수
     print(kwargs)
     # for key, value in kwargs.items():
     #     print(key)
     #     print(value)
-    print(kwargs["multiply"]) # 딕셔너리의 value 뽑기
+    print(kwargs["add"])  # 딕셔너리의 value 뽑기
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
 
 
-calculate(add=3, multiply=20)
+calculate(2, add=3, multiply=20)
+
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw["make"]
+        self.model = kw["model"]
+
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.model)
+
+
+def test(*args):
+    print(args)
+
+
+test(1, 2, 3, 4)
