a51bc66eb008b464b3ea11925766b0ae43b5d7e1
SeungHan Moon <msh9911@naver.com>
2023-11-24 12:24:44 +0900

    Day27

--- Day27/Day27_Start.py
+++ Day27/Day27_Start.py
@@ -1,16 +1,18 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First Tkinter Program")
 window.minsize(500, 300)
 
 #Label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack(side="left")
 
-import turtle
-t = turtle.Turtle()
-t.write("Hi")
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+#Button
+button = Button()
 
 
 window.mainloop()
