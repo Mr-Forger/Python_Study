bf030c5fa2ff19141f4b764d557a1f2dac7d7f65
SeungHan Moon <msh9911@naver.com>
2023-11-28 11:45:02 +0900

    Day28

--- Day28/main.py
+++ Day28/main.py
@@ -14,22 +14,22 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     canvas.itemconfig(timer_text, text=count)
     if count > 0:
         window.after(1000, count_down, count - 1)
 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-def count_down(count):
-    print(count)
-    window.after(1000, count_down, count - 1)
-
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
-count_down(5)
 
 # Label
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
@@ -42,10 +42,8 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-count_down(5)
-
 # Button
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)
