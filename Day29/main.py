7681cb8fddcc692a13dfc4faef99365245e51221
SeungHan Moon <msh9911@naver.com>
2023-12-05 11:31:58 +0900

    Day29 Finish

--- Day29/main.py
+++ Day29/main.py
@@ -5,11 +5,7 @@ import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-               'v',
-               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
-               'R',
-               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -41,11 +37,11 @@ def save():
         messagebox.showinfo(title="Oh no!", message="빈칸을 모두 채워주세요!")
     else:
         is_ok = messagebox.askokcancel(title=website,
-                                       message=f"다음과 같이 저장하시겠습니까?: \nEmail:{email} \nPassword: {password} \n진짜 저장하실꺼죠? ")
+                                       message=f"다음과 같이 저장하시겠습니까?: \nEmail:{email_username} \nPassword: {password} \n진짜 저장하실꺼죠? ")
 
         if is_ok:
             with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+                data_file.write(f"{website} | {email_username} | {password}\n")
                 website_et.delete(0, END)
                 password_et.delete(0, END)
 
